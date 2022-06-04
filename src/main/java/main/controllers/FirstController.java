bd0c36b302a4ae446944d3fb3f355c05b7595d6c
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-06-04 16:02:53 +0300

    Refactor and CleanUpCode

--- src/main/java/main/controllers/FirstController.java
+++ src/main/java/main/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package main.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +23,11 @@ public class FirstController {
     }
 
     @GetMapping("/goodbye-world")
-    public String pageGoodbye(@RequestParam(value = "name",required = false) String name,
-                              @RequestParam(value = "name",required = false) String surname){
+    public String pageGoodbye(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "name", required = false) String surname,
+                              Model model) {
+
+        model.addAttribute("message", "hello" + "   " + name + "        " + surname);
 
         System.out.println("hello" + "   " + name + "        " + surname);
 
