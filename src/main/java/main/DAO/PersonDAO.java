4e902ed13d10de3bd3db89ca75eee52701176a3a
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-07-10 21:32:03 +0300

    work with PeopleController, Person, PersonDAO Make some html File and many other things

--- src/main/java/main/DAO/PersonDAO.java
+++ src/main/java/main/DAO/PersonDAO.java
@@ -1,21 +1,36 @@
 package main.DAO;
 
 import main.models.Person;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Component
 public class PersonDAO {
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person(1,"alex1"));
-        people.add(new Person(2,"alex2"));
-        people.add(new Person(3,"alex3"));
-        people.add(new Person(4,"alex4"));
-        people.add(new Person(5,"alex5"));
+        people.add(new Person(++PEOPLE_COUNT, "alex1"));
+        people.add(new Person(++PEOPLE_COUNT, "alex2"));
+        people.add(new Person(++PEOPLE_COUNT, "alex3"));
+        people.add(new Person(++PEOPLE_COUNT, "alex4"));
+        people.add(new Person(++PEOPLE_COUNT, "alex5"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
--- src/main/java/main/models/Person.java
+++ src/main/java/main/models/Person.java
@@ -4,9 +4,13 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
+
     }
 
     public int getId() {
@@ -24,4 +28,5 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
 }
