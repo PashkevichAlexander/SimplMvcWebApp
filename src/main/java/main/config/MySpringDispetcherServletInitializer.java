0d6358ad37fb0937ae6f43fa12f48408d810d30e
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-07-13 02:02:15 +0300

    work with PeopleController, Person, PersonDAO Make some html File and many other things

--- src/main/java/main/config/MySpringDispetcherServletInitializer.java
+++ src/main/java/main/config/MySpringDispetcherServletInitializer.java
@@ -1,6 +1,10 @@
 package main.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringDispetcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,16 @@ public class MySpringDispetcherServletInitializer extends AbstractAnnotationConf
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
